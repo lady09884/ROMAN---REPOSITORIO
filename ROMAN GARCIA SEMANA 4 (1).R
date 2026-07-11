e0fe7f59cc6b22a04e0960549fc9669fa3ba02ef
LADY ALESSANDRA ROMAN GARCIA <alessandraroman218@gmail.com>
2026-07-11 15:40:34 -0500

    Add files via upload

---
+++ ROMAN GARCIA SEMANA 4 (1).R
@@ -0,0 +1,8 @@
+#SEMANA 4 
+#INSTALANDO 5 PAQUETES 
+install.packages(c("lubridate", "forcats", "caret", "data.table", "reshape2"))
+library(lubridate)
+library(forcats)
+library(caret)
+library(data.table)
+library(reshape2)
